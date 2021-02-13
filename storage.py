62700af9d31b2134eba3b5aa7d2faf897266e6d8
Ocinu <70511389+Ocinu@users.noreply.github.com>
2021-02-13 09:50:15 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -3,7 +3,7 @@ class TextBase:
                  "<a href='https://drive.google.com/file/d/1ue_vYoevG5_PomeVs2tbpF-Mn9ns_6wa/view?usp=sharing'>видео</a>\n" \
                  "\n" \
                  "<b>2</b> <i>03.11</i> Отступы Селекторы\n" \
-                 "<a href=''>видео</a>\n" \
+                 "<a href='https://drive.google.com/file/d/1mMZmw-ROg7CAcXLQ9iIln4oz1ZMlGu4e/view?usp=sharing'>видео</a>\n" \
                  "\n" \
                  "<b>3</b> <i>06.11</i> Графика Анимация Гиперссылки\n" \
                  "<a href='https://drive.google.com/file/d/1CkT07VsgVzi7cCI_iffYNVUkdOqgvamA/view?usp=sharing'>видео</a>\n" \
@@ -19,9 +19,54 @@ class TextBase:
                  "\n" \
                  "<b>7</b> <i>20.11</i> Флексы\n" \
                  "<a href='https://drive.google.com/file/d/1oUncRJfa6nAnfFKmPzPaEXRStRU4T5kq/view?usp=sharing'>видео</a>\n" \
-                 ""
-    HTML_docs = "1 HTML docs\n2 HTML docs\n3 HTML docs\n"
-    HTML_links = "1 HTML link\nhref=\n\n2 HTML link\nhref=\n\n3 HTML link\nhref=\n\n"
+                 "\n" \
+                 "Вернутся в меню выбора раздела: /start"
+    HTML_docs = "<a href='https://drive.google.com/file/d/1Ufn6ombyRbDlpqeLW8SbyF5-3_wlhi90/view?usp=sharing'>HTML Тэги</a>\n" \
+                "<a href='https://drive.google.com/file/d/1ccWX92g3JNQTz1iA50t0L0IvuO8eqCZl/view?usp=sharing'>Отступы Селекторы Псевдоклассы</a>\n" \
+                "<a href='https://drive.google.com/file/d/1Wqn57DDXDBZIxbzYb4DxhqUcTrrYKvMz/view?usp=sharing'>Графика Гиперссылки</a>\n" \
+                "<a href='https://drive.google.com/file/d/1dZoA80I2mjjolY-z0GFP7Q7rDVSF6VWE/view?usp=sharing'>Анимация Transform Списки</a>\n" \
+                "<a href='https://drive.google.com/file/d/1-HGBigoKZRVF2Oy3UxkeVpVnWO_jtRdy/view?usp=sharing'>Таблицы Аудио Видео</a>\n" \
+                "<a href='https://drive.google.com/file/d/13xo7MBRsAn4Ifag4evhvs1dRqW9MnOTd/view?usp=sharing'>Текст</a>\n" \
+                "<a href='https://drive.google.com/file/d/1j_a4LFUQDU_qgcdxNWdpKDGMJtDD0uFH/view?usp=sharing'>Формы Фреймы</a>\n" \
+                "<a href='https://drive.google.com/file/d/1N4YOGEnBhvwPkvikbV6_PJlZDBXxyfVP/view?usp=sharing'>Медиа запросы</a>\n" \
+                "<a href='https://drive.google.com/file/d/1K2ql2A2BKLjF2fANRBsMKiOITnwEJ_09/view?usp=sharing'>Box-model</a>\n" \
+                "<a href='https://drive.google.com/file/d/1vXPH1eMkWThFfm7vtxNZiBYstrVx3arh/view?usp=sharing'>Позиционирование Блоки Флексы</a>\n" \
+                "\n" \
+                "Вернутся в меню выбора раздела: /start"
+    HTML_links = "Лучший англоязычный ресурс по фронтенду\n" \
+                 "https://www.w3schools.com/\n" \
+                 "\n" \
+                 "Русскоязычный справочник, часто устаревшая информация\n" \
+                 "http://htmlbook.ru/\n" \
+                 "\n" \
+                 "Русскоязычный справочник, более актуальный\n" \
+                 "https://webref.ru/\n" \
+                 "\n" \
+                 "Валидация вашей разметки\n" \
+                 "https://validator.w3.org/#validate_by_upload\n" \
+                 "\n" \
+                 "Библиотека иконок\n" \
+                 "https://fontawesome.com/\n" \
+                 "\n" \
+                 "Шрифты\n" \
+                 "https://fonts.google.com/\n" \
+                 "\n" \
+                 "Конвертер цветов\n" \
+                 "https://colorscheme.ru/color-converter.html\n" \
+                 "\n" \
+                 "Редактор SVG графики\n" \
+                 "https://svg-edit.github.io/svgedit/dist/editor/system/index.html\n" \
+                 "\n" \
+                 "Спецсимволы\n" \
+                 "https://pixelplus.ru/studio/stat/html-simvoly-tablitsa-spetssimvolov-v-formate-html/\n" \
+                 "\n" \
+                 "Документация по Emmet\n" \
+                 "https://docs.emmet.io/cheat-sheet/\n" \
+                 "\n" \
+                 "Подключение к верстке Bootstrap\n" \
+                 "https://www.bootstrapcdn.com/\n" \
+                 "\n" \
+                 "Вернутся в меню выбора раздела: /start"
 
     JS_video = "<b>1</b> <i>27.11</i> If Ternary Switch\n" \
                "<a href='https://drive.google.com/file/d/1XExyp-mPpiTSx8mpcyPxBbcmDJRRJ_jA/view?usp=sharing'>видео</a>\n" \
@@ -67,9 +112,35 @@ class TextBase:
                "\n" \
                "<b>15</b> <i>26.01</i> jQuery Ajax\n" \
                "<a href='https://drive.google.com/file/d/1xxifXIUMr6_obwex1HEUBVDg7B0nDn9p/view?usp=sharing'>видео</a>\n" \
-               ""
-    JS_docs = "1 JS docs\n2 JS docs\n3 JS docs\n"
-    JS_links = "1 link\nhref=\n\n2 link\nhref=\n\n3 link\nhref="
+               "\n" \
+               "Вернутся в меню выбора раздела: /start"
+    JS_docs = "<a href='https://drive.google.com/file/d/1y26rYjIIjIrSFwcjGxQnlqvBGeVmTDNd/view?usp=sharing'>Переменные</a>\n" \
+              "<a href='https://drive.google.com/file/d/1LtcoSZWpjAAbhhFIzNBluGZPNVqaBygM/view?usp=sharing'>Массивы Объекты math</a>\n" \
+              "<a href='https://drive.google.com/file/d/1EvOy3awUwbPwZpaCPj_jYT5bs6Mdjhfj/view?usp=sharing'>Функции</a>\n" \
+              "<a href='https://drive.google.com/file/d/1iIgo2LSLGHJgLFqRLfTPEV3y42xV9eHO/view?usp=sharing'>Дата</a>\n" \
+              "<a href='https://drive.google.com/file/d/1DyH6Z8TSejgpld27R-t0kXqiXrwipEiU/view?usp=sharing'>Массивы</a>\n" \
+              "<a href='https://drive.google.com/file/d/13rlV6016-oIKMXerkp0gwtEFrzYELG0b/view?usp=sharing'>Строки</a>\n" \
+              "<a href='https://drive.google.com/file/d/1MEPysgGAytLKxu19-1_StFHpVdAWNzw0/view?usp=sharing'>Объекты Классы</a>\n" \
+              "<a href='https://drive.google.com/file/d/1d4rEH7ERpjMbeDB1y89vkRfdVdqxGqNw/view?usp=sharing'>Обработка событий DOM</a>\n" \
+              "<a href='https://drive.google.com/file/d/1xsVcDCkXMKUxdeteX0vmLy0JM9iuhyHa/view?usp=sharing'>Проверка достоверности форм</a>\n" \
+              "<a href='https://drive.google.com/file/d/1ws-E0XNUToDNEYt5ClTMvTNT7P3Hpq0A/view?usp=sharing'>Регулярные выражения</a>\n" \
+              "<a href='https://drive.google.com/file/d/1yhI_lBEMX5bcFMXcdM-xWQEiaS0FIRL-/view?usp=sharing'>Cookie BOM</a>\n" \
+              "<a href='https://drive.google.com/file/d/11fkGPiI5EAKk-VH0UDMxXZReJuh_n0tX/view?usp=sharing'>Canvas</a>\n" \
+              "<a href='https://drive.google.com/file/d/1vWcffvJ2GJ1rFwe10oEYuFACfEbMrMBU/view?usp=sharing'>Ajax</a>\n" \
+              "<a href='https://drive.google.com/file/d/1CzPZ0y4AOR0zOhBqS_zpNr2dEFH8K0PJ/view?usp=sharing'>jQuery - селекторы</a>\n" \
+              "<a href='https://drive.google.com/file/d/1WiKrqKsoRa_ct7eX0fQeGEnM34iKlkQr/view?usp=sharing'>jQuery - атрибуты, классы, css</a>\n" \
+              "<a href='https://drive.google.com/file/d/1fJm1_apfEmu2Z-B7BPBR4URiZRP-_cbS/view?usp=sharing'>jQuery - события</a>\n" \
+              "<a href='https://drive.google.com/file/d/1Aj8pYysRnCGxka1svsrwIz9zHqhWDDsY/view?usp=sharing'>jQuery - взаимодействие с DOM</a>\n" \
+              "<a href='https://drive.google.com/file/d/1K5QbdlnfEJd74Am4gsc6SJCQVETI1eT-/view?usp=sharing'>jQuery - Ajax</a>\n" \
+              "\n" \
+              "Вернутся в меню выбора раздела: /start"
+    JS_links = "Плагины для jQuery\n" \
+               "https://tutorialzine.com/2013/04/50-amazing-jquery-plugins\n" \
+               "\n" \
+               "Видеокурс из 100 уроков по React\n" \
+               "https://www.youtube.com/playlist?list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8\n" \
+               "\n" \
+               "Вернутся в меню выбора раздела: /start"
 
     Python_video = "<b>1</b> <i>29.01</i> Средства разработки Переменные\n" \
                    "<a href='https://drive.google.com/file/d/17e2Tv-JCwqmBSeUxxW_46k5xD4keRkGU/view?usp=sharing'>видео</a>\n" \
@@ -82,9 +153,54 @@ class TextBase:
                    "\n" \
                    "<b>4</b> <i>09.02</i> Строки\n" \
                    "<a href='https://drive.google.com/file/d/1pCCGA1Turer2xqyXpDXH4pgCiFF0P-Fh/view?usp=sharing'>видео</a>\n" \
-                   ""
-    Python_docs = "1 Python docs\n2 Python docs\n3 Python docs\n"
-    Python_links = "1 Python link\nhref=\n\n2 Python link\nhref=\n\n3 Python link\nhref="
+                   "\n" \
+                   "<b>5</b> <i>12.02</i> Функции\n" \
+                   "<a href='https://drive.google.com/file/d/1Nv_Tj598NKrNyln0_Ru0BsqbExJRtjgn/view?usp=sharing'>видео</a>\n" \
+                   "\n" \
+                   "Вернутся в меню выбора раздела: /start"
+    Python_docs = "1 Python docs\n2 Python docs\n3 Python docs\n" \
+                  "\n" \
+                  "Вернутся в меню выбора раздела: /start"
+    Python_links = "Самоучитель  по Python на русском\n" \
+                   "https://pythonworld.ru/samouchitel-python\n" \
+                   "\n" \
+                   "Самоучитель на русском\n" \
+                   "https://younglinux.info/\n" \
+                   "\n" \
+                   "Руководство по Python\n" \
+                   "https://metanit.com/python/tutorial/\n" \
+                   "\n" \
+                   "Статьи про Python на Хабре\n" \
+                   "https://habr.com/ru/search/?q=%5Bpython%5D\n" \
+                   "\n" \
+                   "Русскоязычный stackoverflow для питонистов\n" \
+                   "https://ru.stackoverflow.com/questions/tagged/python\n" \
+                   "\n" \
+                   "Руссоязычный сайт питонистов\n" \
+                   "https://pythonist.ru/\n" \
+                   "\n" \
+                   "Документация PEP8 на русском\n" \
+                   "https://pep8.ru/doc/pep8/\n" \
+                   "\n" \
+                   "Библиотека модулей\n" \
+                   "https://pypi.org/\n" \
+                   "\n" \
+                   "\n" \
+                   "<b>Книги:</b>\n" \
+                   "Книги по прикладному использованию Python\n" \
+                   "https://techrocks.ru/2021/01/29/books-about-using-python-in-practice/\n" \
+                   "\n" \
+                   "Топ-7 книг по алгоритмам на русском языке\n" \
+                   "https://techrocks.ru/2020/07/09/top-7-books-about-algorithms-in-russian/\n" \
+                   "\n" \
+                   "Чистый код" \
+                   "https://www.fita.farm/wp-content/uploads/2017/10/Chisty_kod_Sozdanie_analiz_i_refaktoring.pdf" \
+                   "\n" \
+                   "\n" \
+                   "<b>Видео:</b>\n" \
+                   "<a href='https://www.youtube.com/watch?v=hjdRQspuelk&ab_channel=%D0%94%D0%B8%D0%B4%D0%B6%D0%B8%D1%82%D0%B0%D0%BB%D0%B8%D0%B7%D0%B8%D1%80%D1%83%D0%B9%21'>Именование переменных, классов и методов в коде</a>\n" \
+                   "\n" \
+                   "Вернутся в меню выбора раздела: /start"
 
     # def __init__(self):
     #     self.HTML_video = [
